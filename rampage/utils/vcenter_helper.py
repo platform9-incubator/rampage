13375976f763c8dadea0d247bd617bdc128f5d4c
Arun Sriraman <sarun87@gmail.com>
2016-09-13 16:01:26 -0700

    Added more helper functions to VCenter Helper

--- rampage/utils/vcenter_helper.py
+++ rampage/utils/vcenter_helper.py
@@ -2,13 +2,14 @@ from pyVim import connect
 from pyVmomi import vim
 from pyVmomi import vmodl
 
-import atexit
 import ssl
 import os
+import logging
+
+LOG = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 class VCHelper(object):
-    envs = {}
-    si = None
 
     def load_vcenter_info_from_env(self):
         """
@@ -24,8 +25,18 @@ class VCHelper(object):
         """
         Initialization
         """
+        self.envs = {}
+        self.si = None
         self.load_vcenter_info_from_env()
+        self.vmobj_cache = {}
 
+    def __del__(self):
+        """
+        Clean stuff up
+        :return:
+        """
+        if self.si is not None:
+            connect.Disconnect(self.si)
 
     def get_si(self):
         """
@@ -40,7 +51,6 @@ class VCHelper(object):
                                                     pwd=self.envs['vsphere_pass'],
                                                     port=self.envs['vsphere_port'])
             ssl._create_default_https_context = default_context
-            atexit.register(connect.Disconnect, self.si)
 
         except vmodl.MethodFault as error:
             print "Caught vmodl fault : " + error.msg
@@ -53,13 +63,49 @@ class VCHelper(object):
         :param ipaddr: IP Address as a string
         :return: VM Context if the VM with IP exists else None.
         """
+        # Check if vm object already in cache. If yes, return the same object.
+        if ipaddr in self.vmobj_cache and self.vmobj_cache[ipaddr] is not None:
+            return self.vmobj_cache[ipaddr]
         res = None
         try:
             if self.si is None:
                 self.get_si()
 
-            content = self.si.RetrieveContent()
-            res = content.searchIndex.FindByIp(ip=ipaddr, vmSearch=True)
+            #content = self.si.RetrieveContent()
+            res = self.si.content.searchIndex.FindByIp(ip=ipaddr, vmSearch=True)
+        except vmodl.RuntimeFault as error:
+            print "Caught vmodl fault:" + error.msg
+        self.vmobj_cache[ipaddr] = res
+        return res
+
+    def get_vm_from_name(self, name='arun-cirros-breakathon'):
+        """
+        Return VM context given the vm dns name.
+        :param name: string name of vm
+        :return: VM Context if the VM with name exists else None.
+        """
+        try:
+            if self.si is None:
+                self.get_si()
+
+            #content = self.si.RetrieveContent()
+            res = self.si.content.searchIndex.FindByDnsName(dnsName=name, vmSearch=True)
+        except vmodl.RuntimeFault as error:
+            print "Caught vmodl fault:" + error.msg
+        return res
+
+    def get_vm_from_uuid(self, uuid):
+        """
+        Return VM context given the vm uuid.
+        :param name: string uuid of vm
+        :return: VM Context if the VM with uuid exists else None.
+        """
+        try:
+            if self.si is None:
+                self.get_si()
+
+            #content = self.si.RetrieveContent()
+            res = self.si.content.searchIndex.FindByUuid(uuid=uuid, vmSearch=True,instanceUuid=True)
         except vmodl.RuntimeFault as error:
             print "Caught vmodl fault:" + error.msg
         return res
@@ -70,3 +116,26 @@ class VCHelper(object):
         :param vm_object: vm object for which we need to power off.
         :return:
         """
+        LOG.debug("Powering off VM")
+        vm_object.PowerOff()
+        LOG.debug("Powered off VM")
+
+    def power_on_vm(self, vm_object):
+        """
+
+        :param vm_object:
+        :return:
+        """
+        LOG.debug("Powering on VM")
+        vm_object.PowerOn()
+        LOG.debug("Powered on VM")
+
+    def hard_reboot_vm(self, vm_object):
+        """
+
+        :param vm_object:
+        :return:
+        """
+        LOG.debug("Rebooting VM")
+        res = vm_object.Reset()
+        LOG.debug("Rebooted VM")
